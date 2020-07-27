9ea852c9b7c0d8af8136355707d040f24c40ce0a
vladimiremi <vladimiremi2014@gmail.com>
2020-07-26 21:09:33 -0300

    Projeto Backend Funcional

---
+++ backend/src/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { uuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+    const { title } = request.query;
+
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+    const {title, owner} = request.body;
+    const project = {id: uuid(), title, owner};
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+    const projectIndex = projects.findIndex(project => project.id == id);
+    console.log(projectIndex);
+    if (projectIndex < 0){
+        return response.status(400).json({error: 'Not found project.'});
+    };
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+    console.log(projectIndex);
+
+    if (projectIndex < 0){
+        return response.status(400).json({error: "Not found project"});
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send({accept: "Deletado!"});
+});
+
+
+app.listen(3333, () => {
+    console.log('✔ Back-end Started!');
+});
